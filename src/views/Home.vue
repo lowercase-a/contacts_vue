7a9489a5ad7074973086659ef41d2138247fb215
Brian <riceb53@gmail.com>
2019-04-07 10:24:58 -0500

    add contacts index

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <div v-for="contact in contacts">
+      {{ contact.full_name }}
+      {{ contact.email }}
+      <hr>
+    </div>
   </div>
 </template>
 
+<style>
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  import axios from "axios";
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!",
+      contacts: []
+    };
+  },
+  created: function() {
+    axios.get("/api/contacts").then(response => {
+      console.log(response);
+      this.contacts = response.data;
+    });
+  },
+  methods: {}
+};
 </script>
+
