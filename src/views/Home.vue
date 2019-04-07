a147e298fe92baa6c6b8d701cae20388051b3866
Brian <riceb53@gmail.com>
2019-04-07 10:42:54 -0500

    contacts new/create action

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <p>firstName: <input type="text" v-model="newContact.firstName"></p>
+    <p>lastName: <input type="text" v-model="newContact.lastName"></p>
+    <p>email: <input type="text" v-model="newContact.email"></p>
+    <p>phoneNumber: <input type="text" v-model="newContact.phoneNumber"></p>
+    <p>middleName: <input type="text" v-model="newContact.middleName"></p>
+    <p>bio: <input type="text" v-model="newContact.bio"></p>
+    <button v-on:click="makeContact()">Make the contact</button>
     <div v-for="contact in contacts">
       {{ contact.full_name }}
       {{ contact.email }}
@@ -19,7 +26,15 @@ export default {
   data: function() {
     return {
       message: "Welcome to Vue.js!",
-      contacts: []
+      contacts: [],
+      newContact: {
+        firstName: "",
+        lastName: "",
+        email: "",
+        phoneNumber: "",
+        middleName: "",
+        bio: ""
+      }
     };
   },
   created: function() {
@@ -28,7 +43,26 @@ export default {
       this.contacts = response.data;
     });
   },
-  methods: {}
+  methods: {
+    makeContact: function() {
+      console.log('making the contact...');
+      // collect all the data
+      var params = {
+        first_name: this.newContact.firstName,
+        last_name: this.newContact.lastName,
+        email: this.newContact.email,
+        phone_number: this.newContact.phoneNumber,
+        bio: this.newContact.bio,
+        middle_name: this.newContact.middleName
+      }
+      // send the data to the api
+      axios.post("/api/contacts", params).then(response => {
+        console.log(response);
+        // add the data to the array
+        this.contacts.push(response.data)
+      });
+    }
+  }
 };
 </script>
 
